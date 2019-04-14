8bf7fa5fddd7456b72b9b92adf800160e33bf8bc
Oleksandr Dievri <o.dievri@outlook.com>
2019-04-15 00:36:35 +0300

    add arrays slices dir

--- 02_args/main.go
+++ 02_args/main.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
+var lala = "ssu"
+
 func main() {
 	var name string = "Sasha"
-	fmt.Println(name)
+	var age int = 37
+	var isCool = true
+	isCool = false
+	kuku := "Putu"
+	size := 3.72
+	name, email := "Brad", "sasha@brad.tr"
+	fmt.Println(lala, kuku)
+	fmt.Println(name, age, isCool, email)
+	fmt.Printf("%T\n", size)
 }
---
+++ 03_packages/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dievri/go_project/03_packages/strutil"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(math.Ceil(2.7))
+	fmt.Println(strutil.Reverse("pello"))
+}
